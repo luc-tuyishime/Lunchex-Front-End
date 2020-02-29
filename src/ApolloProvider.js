371e853db6d8b964dd11ecfc5707757d3e52fde4
Jean luc Tuyishime <jeanluctuyishime@Jeans-MacBook-Pro.local>
2020-02-29 21:40:03 +0200

    edited proxy and base URL

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -7,7 +7,7 @@ import { ApolloProvider } from '@apollo/react-hooks';
 import { setContext } from 'apollo-link-context';
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:5000'
+  uri: 'https://evening-inlet-23567.herokuapp.com/'
 });
 
 // ADD TOKEN TO OUR REQUEST LIKE POSTING
